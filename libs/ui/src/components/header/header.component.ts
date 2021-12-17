1234a177c80d562043bd10ca1519f53cb3dd3166
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-12-18 00:56:43 +0530

    feat(header): add search input bar

--- libs/ui/src/components/header/header.component.ts
+++ libs/ui/src/components/header/header.component.ts
@@ -2,30 +2,39 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IconModule } from '../icon.module';
+
 @Component({
   selector: 'wfh-header',
   template: `
-    <header class="h-20 flex justify-between items-center px-4 md:px-6 max-w-7xl mx-auto">
+    <header
+      class="bg-white z-10 h-20 flex justify-between items-center px-4 md:px-6 max-w-7xl mx-auto"
+    >
       <div class="flex items-center gap-2">
         <img src="https://avatar.tobi.sh/tobiaslins?size=30" alt="" />
         <p class="text-gray-600 font-bold">My WFH Setup</p>
-      </div>
-      <nav>
-        <ul class="flex items-center gap-8">
-          <ng-container *ngFor="let item of links">
-            <li
-              class="text-gray-500 hover:text-primary cursor-pointer transition-colors duration-200"
-            >
-              <a
-                [routerLink]="item.link"
-                routerLinkActive="text-primary"
-                [routerLinkActiveOptions]="{ exact: true }"
-                >{{ item.label }}</a
+        <nav class="ml-4">
+          <ul class="flex items-center gap-8">
+            <ng-container *ngFor="let item of links">
+              <li
+                class="text-gray-500 hover:text-primary cursor-pointer transition-colors duration-200"
               >
-            </li>
-          </ng-container>
-        </ul>
-      </nav>
+                <a
+                  [routerLink]="item.link"
+                  routerLinkActive="text-primary"
+                  [routerLinkActiveOptions]="{ exact: true }"
+                  >{{ item.label }}</a
+                >
+              </li>
+            </ng-container>
+          </ul>
+        </nav>
+      </div>
+      <div class="flex-1 px-6 relative">
+        <div class="absolute h-full left-8 top-0 grid place-items-center">
+          <rmx-icon name="search-2-line"></rmx-icon>
+        </div>
+        <input class="w-full pl-8" type="search" name="" id="" />
+      </div>
       <div class="flex gap-2">
         <button class="header__icon">
           <rmx-icon name="notification-4-fill"></rmx-icon>
@@ -40,7 +49,8 @@ import { IconModule } from '../icon.module';
     `
       .header__icon {
         @apply rounded-full hover:bg-gray-200 p-2;
-        &:hover {
+
+        & :hover {
           .rmx-icon {
             @apply fill-primary;
           }
