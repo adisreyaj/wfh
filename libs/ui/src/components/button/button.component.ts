e3876eaefe4213d48076e26d4829bf6ba4970fd4
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-12-18 00:36:54 +0530

    style(components): add size option in button

--- libs/ui/src/components/button/button.component.ts
+++ libs/ui/src/components/button/button.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostBinding, Input, NgModule } from '@angular/core';
+
 @Component({
-  selector: 'button',
+  selector: 'button[wfh]',
   template: ` <ng-content></ng-content> `,
   styles: [
     `
@@ -10,32 +11,49 @@ import { ChangeDetectionStrategy, Component, HostBinding, Input, NgModule } from
         @apply transition duration-150 ease-in-out;
       }
 
+      :host-context(.xsmall) {
+        @apply px-2 py-1 text-xs font-medium;
+      }
+
+      :host-context(.small) {
+        @apply px-2 py-2 text-sm font-medium;
+      }
+
       :host-context(.primary) {
         @apply bg-primary text-white;
         @apply hover:bg-primary-dark;
       }
+
       :host-context(.secondary) {
         @apply bg-secondary text-white;
       }
+
       :host-context(.neutral) {
         @apply bg-gray-100 hover:bg-gray-200 hover:text-primary;
+      }
+
+      :host-context(.outline) {
+        @apply hover:bg-gray-100 outline-none border border-gray-200 hover:text-primary;
       }
     `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
+  @Input()
+  class = '';
+  @Input()
+  variant = 'primary';
+
+  @Input()
+  size = 'base';
+
   @HostBinding('class')
   get buttonClasses() {
-    const baseClasses = new Set(['button', this.variant ?? 'primary']);
+    const baseClasses = new Set(['button', this.variant ?? 'primary', this.size]);
     this.class.split(' ').forEach((c) => baseClasses.add(c));
     return Array.from(baseClasses).join(' ');
   }
-  @Input()
-  class = '';
-
-  @Input()
-  variant = 'primary';
 }
 
 @NgModule({
