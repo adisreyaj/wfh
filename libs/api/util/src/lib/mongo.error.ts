04ac5b21487b595583411dc2b6e76f43d23aafbb
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-12-21 22:36:56 +0530

    feat(utils): add mongo error handler custom operator

--- libs/api/util/src/lib/mongo.error.ts
+++ libs/api/util/src/lib/mongo.error.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, HttpException, InternalServerErrorException } from '@nestjs/common';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 export const MONGO_ERROR = (...args: string[]): Record<string, HttpException> => ({
   11000: new BadRequestException(`${args[0]} already exists`),
@@ -9,9 +9,11 @@ export const handleError = (entity: string) => (source: Observable<unknown>) =>
   source.pipe(
     catchError((err) => {
       if (err instanceof HttpException) {
-        return of(err);
+        return throwError(() => err);
       }
       const exception = MONGO_ERROR(entity)[err.code as string];
-      return exception != null ? of(exception) : of(new InternalServerErrorException(err));
+      return exception != null
+        ? throwError(() => exception)
+        : throwError(() => new InternalServerErrorException(err));
     })
   );
