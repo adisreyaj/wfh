90932a5769d8342964036c1d5eab4bc1590da168
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-12-27 20:09:56 +0530

    feat(orders-api): add orders controller

--- apps/api/src/app/orders/orders.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrdersController } from './orders.controller';
-import { OrdersService } from './orders.service';
-
-@Module({
-  controllers: [OrdersController],
-  providers: [OrdersService]
-})
-export class OrdersModule {}
