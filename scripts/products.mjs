c7be1bc51b680c5c8b4d32d096150629107d9232
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-08 17:21:30 +0530

    feat(scripts): update seed scripts

--- scripts/products.mjs
+++ scripts/products.mjs
@@ -1,1 +1,34 @@
-export const PRODUCTS = [{}]
+const KEYBOARDS = [
+  {
+    name: 'K2 (Hot-Swappable) Version 2',
+    price: 7840,
+    description: '',
+    images: [
+      'https://keychron.in/wp-content/uploads/2021/04/Keychron-K2-hot-swappable-wireless-mechanical-keyboard-for-Mac-Windows-iOS-Gateron-switch-brown-with-type-C-RGB-white-backlight_1800x1800-300x300.jpg',
+    ],
+    category: 'Keyboard',
+    brand: 'Keychron',
+    colors: ['black', 'white'],
+    type: 'Wireless',
+    numberOfKeys: 84,
+    illumination: 'RGB',
+    keyCaps: 'PBT',
+  },
+  {
+    name: 'Keychron K8 Wireless Mechanical Keyboard',
+    price: 8999,
+    description: '',
+    images: [
+      'https://keychron.in/wp-content/uploads/2021/04/Keychron-K2-hot-swappable-wireless-mechanical-keyboard-for-Mac-Windows-iOS-Gateron-switch-brown-with-type-C-RGB-white-backlight_1800x1800-300x300.jpg',
+    ],
+    category: 'Keyboard',
+    brand: 'Keychron',
+    colors: ['black', 'white'],
+    type: 'Wireless',
+    numberOfKeys: 84,
+    illumination: 'LED',
+    keyCaps: 'ABS',
+  },
+];
+
+export const PRODUCTS = [{ items: KEYBOARDS, kind: 'ProductKeyboard' }];
--- scripts/seed.mjs
+++ scripts/seed.mjs
@@ -2,47 +2,173 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import { BRANDS } from './brands.mjs';
 import { CATEGORIES } from './categories.mjs';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { PRODUCTS } from './products.mjs';
 
-const seedBrands = async () => {
-  try {
-    await Promise.all(
-      BRANDS.map((brand) =>
-        fetch('http://localhost:3333/api/brands', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(brand),
-        })
-      )
-    );
-    console.log(chalk.green('Brands seeded successfully'));
-  } catch (e) {
-    console.log(chalk.red('Error seeding brands', e));
-  }
-};
-const seedCategories = async () => {
-  try {
-    await Promise.all(
-      CATEGORIES.map((category) =>
-        fetch('http://localhost:3333/api/categories', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(category),
-        })
-      )
-    );
-    console.log(chalk.green('Categories seeded successfully'));
-  } catch (e) {
-    console.log(chalk.red('Error seeding categories', e));
-  }
-};
+dotenv.config({
+  path: '../.env',
+});
 
-const seed = async () => {
-  await seedBrands();
-  await seedCategories();
-};
+class Seeder {
+  AUTH_HEADER = {
+    'x-internal-token': `${process.env.INTERNAL_TOKEN}`,
+  };
 
-await seed();
+  status = {
+    seedStatus: {
+      brand: false,
+      category: false,
+      product: false,
+    },
+  };
+  brands = null;
+  categories = null;
+
+  constructor(props) {}
+
+  #loadStatusFile = async () => {
+    try {
+      const status = await fs.promises.readFile('./result.json', 'utf8');
+      return JSON.parse(status);
+    } catch (e) {
+      return null;
+    }
+  };
+
+  #seedBrands = async () => {
+    if (this.status.seedStatus.brand) return;
+    try {
+      await Promise.all(
+        BRANDS.map((brand) =>
+          fetch('http://localhost:3333/api/brands', {
+            method: 'POST',
+            headers: {
+              ...this.AUTH_HEADER,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(brand),
+          })
+        )
+      );
+      console.log(chalk.green('Brands seeded successfully'));
+    } catch (e) {
+      console.log(chalk.red('Error seeding brands', e));
+    }
+  };
+
+  #seedCategories = async () => {
+    if (this.status.seedStatus.category) return;
+
+    try {
+      await Promise.all(
+        CATEGORIES.map((category) =>
+          fetch('http://localhost:3333/api/categories', {
+            method: 'POST',
+            headers: {
+              ...this.AUTH_HEADER,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(category),
+          })
+        )
+      );
+      console.log(chalk.green('Categories seeded successfully'));
+    } catch (e) {
+      console.log(chalk.red('Error seeding categories', e));
+    }
+  };
+
+  #getBrands = async () => {
+    try {
+      const response = await fetch('http://localhost:3333/api/brands', {
+        headers: {
+          ...this.AUTH_HEADER,
+        },
+      });
+      const data = await response.json();
+      console.log(chalk.green('Brands fetched successfully'));
+      return data;
+    } catch (e) {
+      console.log(chalk.red('Error fetching brands', e));
+    }
+  };
+
+  #getCategories = async () => {
+    try {
+      const response = await fetch('http://localhost:3333/api/categories', {
+        headers: {
+          ...this.AUTH_HEADER,
+        },
+      });
+      const data = await response.json();
+      console.log(chalk.green('Categories fetched successfully'));
+      return data;
+    } catch (e) {
+      console.log(chalk.red('Error fetching categories', e));
+    }
+  };
+
+  #getExistingData = async () => {
+    const brands = (await this.#getBrands()) ?? [];
+    this.brands = brands.map((brand) => ({
+      name: brand.name,
+      id: brand._id,
+    }));
+    await fs.promises.writeFile('./brands.json', JSON.stringify(this.brands));
+    const categories = (await this.#getCategories()) ?? [];
+    this.categories = categories.map((brand) => ({
+      name: brand.name,
+      id: brand._id,
+    }));
+    await fs.promises.writeFile('./categories.json', JSON.stringify(this.categories));
+  };
+
+  seed = async () => {
+    this.status = await this.#loadStatusFile();
+    await this.#getExistingData();
+    if (this.status) {
+      await this.#seedBrands();
+      await this.#seedCategories();
+      await this.#seedProducts();
+    }
+  };
+
+  #seedProducts = async () => {
+    if (this.status.seedStatus.product) return;
+    try {
+      await Promise.all(
+        PRODUCTS.reduce((acc, curr) => {
+          acc.push(
+            curr.items.map((product) => {
+              const brand = this.brands.find((brand) => brand.name === product.brand);
+              const category = this.categories.find(
+                (category) => category.name === product.category
+              );
+              return fetch('http://localhost:3333/api/products', {
+                method: 'POST',
+                headers: {
+                  ...this.AUTH_HEADER,
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  ...product,
+                  kind: curr.kind,
+                  brand: brand.id,
+                  category: category.id,
+                }),
+              });
+            })
+          );
+          return acc;
+        }, [])
+      );
+      console.log(chalk.green('Products seeded successfully'));
+    } catch (e) {
+      console.log(chalk.red('Error seeding products', e));
+    }
+  };
+}
+
+const seeder = new Seeder();
+await seeder.seed();
