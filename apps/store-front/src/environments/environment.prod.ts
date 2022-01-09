f46a96cae202760a174a574c9a645f10937cbcb9
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-09 18:02:05 +0530

    chore: update env

--- apps/store-front/src/environments/environment.prod.ts
+++ apps/store-front/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://wfh-store-api.adi.so/api',
+  auth: {
+    audience: 'wfh-store-api',
+    domain: 'wfh-dev.us.auth0.com',
+    clientId: 'cGy0vVAZXK91DZ4EK5jcf9hP6d8yiPN7',
+    redirectUri: 'http://localhost:4200',
+  },
 };
