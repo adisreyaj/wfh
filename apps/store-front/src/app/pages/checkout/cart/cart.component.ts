a4588f2285bf1dc852d68ec23d1f36a968d88366
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-10 22:38:52 +0530

    feat(cart): empty cart state addded

--- apps/store-front/src/app/pages/checkout/cart/cart.component.ts
+++ apps/store-front/src/app/pages/checkout/cart/cart.component.ts
@@ -14,87 +14,105 @@ import { HotToastService } from '@ngneat/hot-toast';
 @Component({
   selector: 'wfh-cart',
   template: `
-    <section class="flex-1">
-      <section class="cart__section">
-        <header class="mb-4">
-          <h2 class="text-xl font-bold text-gray-500">In Cart</h2>
-        </header>
-        <section class="grid xl:grid-cols-2 gap-4">
-          <article class="" *ngFor="let item of items$ | async">
-            <wfh-cart-item [product]="item" (delete)="onDelete(item)"></wfh-cart-item>
-          </article>
-        </section>
-      </section>
-      <ng-container *ngIf="auth.isAuthenticated$ | async; else login">
-        <section class="cart__section" #address>
+    <ng-container *ngIf="items$ | async as items">
+      <ng-container *ngIf="items.length > 0; else emptyCart">
+        <section class="flex-1">
           <header class="mb-4">
-            <h2 class="text-xl font-bold text-gray-500">Address</h2>
+            <h2 class="text-xl font-bold text-gray-500">In Cart</h2>
           </header>
-          <div class="grid lg:grid-cols-2 gap-4">
-            <ng-container *ngFor="let address of addresses$ | async">
-              <article
-                (click)="step = 1; addressSelected = address.id"
-                class="border cursor-pointer text-gray-600 hover:shadow-lg hover:-translate-y-1 relative border-gray-200 p-4"
-              >
-                <div class="absolute top-2 right-2">
-                  <wfh-checkbox [checked]="addressSelected === address.id"></wfh-checkbox>
-                </div>
-                <p>{{ address?.apartment }}, {{ address?.street }}</p>
-                <p>{{ address?.city }}</p>
-                <p>{{ address?.state }}, {{ address?.country }}</p>
-                <p class="font-semibold">{{ address?.zip }}</p>
-                <p>{{ address?.phone }}</p>
+          <section class="cart__section">
+            <section class="grid xl:grid-cols-2 gap-4">
+              <article class="" *ngFor="let item of items">
+                <wfh-cart-item [product]="item" (delete)="onDelete(item)"></wfh-cart-item>
               </article>
-            </ng-container>
-          </div>
-        </section>
-        <section class="cart__section" #payments *ngIf="step > 0">
-          <header class="mb-4">
-            <h2 class="text-xl font-bold text-gray-500">Payment</h2>
-          </header>
-          <div class="grid lg:grid-cols-3 gap-4">
-            <ng-container *ngFor="let card of cards$ | async; index as i">
-              <label
-                (click)="step = 2"
-                [for]="'card=' + i"
-                class="text-gray-600 relative hover:-translate-y-1 hover:shadow-lg cursor-pointer transition-all duration-200"
-              >
-                <div class="absolute top-2 right-2">
-                  <input type="radio" name="card" [id]="'card=' + i" />
-                </div>
-                <wfh-credit-card
-                  [attr.data-id]="i + 1"
-                  [number]="card.number"
-                  [expiry]="card.expiry"
-                  [name]="card.name"
-                ></wfh-credit-card>
-              </label>
-            </ng-container>
-          </div>
-        </section>
-      </ng-container>
-      <ng-template #login>
-        <section class="flex flex-col items-center py-4">
-          <img src="assets/images/girl-cart.svg" alt="SHopping" class="h-60" />
-          <p class="mb-4 text-md">
-            Please login or signup to continue.<br />
-            <span class="text-gray-500 text-sm"> You will need to choose/add an address.</span>
-          </p>
+            </section>
+          </section>
+          <ng-container *ngIf="auth.isAuthenticated$ | async; else login">
+            <section class="cart__section" #address>
+              <header class="mb-4">
+                <h2 class="text-xl font-bold text-gray-500">Address</h2>
+              </header>
+              <div class="grid lg:grid-cols-2 gap-4">
+                <ng-container *ngFor="let address of addresses$ | async">
+                  <article
+                    (click)="step = 1; addressSelected = address.id"
+                    class="border cursor-pointer text-gray-600 hover:shadow-lg hover:-translate-y-1 relative border-gray-200 p-4"
+                  >
+                    <div class="absolute top-2 right-2">
+                      <wfh-checkbox [checked]="addressSelected === address.id"></wfh-checkbox>
+                    </div>
+                    <p>{{ address?.apartment }}, {{ address?.street }}</p>
+                    <p>{{ address?.city }}</p>
+                    <p>{{ address?.state }}, {{ address?.country }}</p>
+                    <p class="font-semibold">{{ address?.zip }}</p>
+                    <p>{{ address?.phone }}</p>
+                  </article>
+                </ng-container>
+              </div>
+            </section>
+            <section class="cart__section" #payments *ngIf="step > 0">
+              <header class="mb-4">
+                <h2 class="text-xl font-bold text-gray-500">Payment</h2>
+              </header>
+              <div class="grid lg:grid-cols-3 gap-4">
+                <ng-container *ngFor="let card of cards$ | async; index as i">
+                  <label
+                    (click)="step = 2"
+                    [for]="'card=' + i"
+                    class="text-gray-600 relative hover:-translate-y-1 hover:shadow-lg cursor-pointer transition-all duration-200"
+                  >
+                    <div class="absolute top-2 right-2">
+                      <input type="radio" name="card" [id]="'card=' + i" />
+                    </div>
+                    <wfh-credit-card
+                      [attr.data-id]="i + 1"
+                      [number]="card.number"
+                      [expiry]="card.expiry"
+                      [name]="card.name"
+                    ></wfh-credit-card>
+                  </label>
+                </ng-container>
+              </div>
+            </section>
+          </ng-container>
+          <ng-template #login>
+            <section class="flex flex-col items-center py-4">
+              <img src="assets/images/girl-cart.svg" alt="SHopping" class="h-60" />
+              <p class="mb-4 text-md">
+                Please login or signup to continue.<br />
+                <span class="text-gray-500 text-sm"> You will need to choose/add an address.</span>
+              </p>
 
-          <button wfh (click)="auth.loginWithPopup()">Login or Signup</button>
+              <button wfh (click)="auth.loginWithPopup()">Login or Signup</button>
+            </section>
+          </ng-template>
         </section>
-      </ng-template>
-    </section>
-    <aside class="cart__sidebar">
-      <header class="mb-4">
-        <h2 class="text-xl font-bold text-gray-500">Summary</h2>
-      </header>
-      <wfh-cart-sidebar
-        (clicked)="onClicked()"
-        [state]="step"
-        [priceBreakdown]="priceBreakdown$ | async"
-      ></wfh-cart-sidebar>
-    </aside>
+        <aside class="cart__sidebar">
+          <header class="mb-4">
+            <h2 class="text-xl font-bold text-gray-500">Summary</h2>
+          </header>
+          <wfh-cart-sidebar
+            (clicked)="onClicked()"
+            [state]="step"
+            [priceBreakdown]="priceBreakdown$ | async"
+          ></wfh-cart-sidebar>
+        </aside>
+      </ng-container>
+    </ng-container>
+
+    <ng-template #emptyCart>
+      <section class="flex flex-col items-center w-full h-full">
+        <img src="assets/images/girl-shopping.svg" alt="Shopping" [style.height.px]="500" />
+        <p class="mb-4 text-lg text-center font-semibold">
+          Your cart is empty.<br />
+          <span class="text-gray-500 text-sm">
+            Find your favorite products and add them to your cart.</span
+          >
+        </p>
+
+        <button wfh routerLink="/products">Browser Products</button>
+      </section>
+    </ng-template>
   `,
   styles: [
     // language=SCSS
