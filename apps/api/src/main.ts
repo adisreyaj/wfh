4f7d8b0569efff5dc4432b133b28e3111a093240
Adithya Sreyaj <adi.sreyaj@gmail.com>
2024-04-10 20:02:39 +0530

    chore:add nixpacks

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['localhost:4200', '*adi.so'],
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
